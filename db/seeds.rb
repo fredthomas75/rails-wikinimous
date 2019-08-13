814fba397357c850801bec1bb03203f1fad6f158
Frederic Thomas <fred@mt2lab.com>
2019-08-13 17:07:50 -0400

    Wiki

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times { Article.create(title: Faker::Book.title, content: Faker::Quote.most_interesting_man_in_the_world) }
